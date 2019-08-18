9ad5215be3a3bda0759fc4e662c923e0dcecd2a4
dead-beef <dead-beef@users.noreply.github.com>
2019-08-18 19:06:55 +0000

    fix sqlite functions

--- .bot/scripts/sqlite_functions.py
+++ .bot/scripts/sqlite_functions.py
@@ -1,23 +1,26 @@
 import math
-from inspect import signature
+from itertools import chain
 
 
-MATH = dict(
-    (name, getattr(math, name))
-    for name in (
-        'acosh', 'acos', 'asinh', 'asin', 'atanh', 'atan',
-        'ceil', 'cos', 'cosh', 'degrees', 'erfc', 'erf',
-        'expm1', 'exp', 'fabs', 'floor', 'fmod', 'frexp',
-        'fsum', 'gamma', 'isfinite', 'isinf',
-        'isnan', 'lgamma', 'log10',
-        'log1p', 'log2', 'log', 'modf', 'pow', 'radians',
-        'sinh', 'sin', 'sqrt', 'tanh', 'tan', 'trunc'
-    )
-)
+MATH = list(chain.from_iterable(
+    ((func, nargs, getattr(math, func)) for func in funcs)
+    for nargs, funcs in enumerate((
+        (
+            'acosh', 'acos', 'asinh', 'asin', 'atanh', 'atan',
+            'ceil', 'cos', 'cosh', 'degrees', 'erfc', 'erf',
+            'expm1', 'exp', 'fabs', 'floor', 'fmod', 'frexp',
+            'fsum', 'gamma', 'isfinite', 'isinf',
+            'isnan', 'lgamma', 'log10',
+            'log1p', 'log2', 'log', 'modf', 'radians',
+            'sinh', 'sin', 'sqrt', 'tanh', 'tan', 'trunc'
+        ),
+        ('log', 'pow')
+    ), 1)
+))
 
 
 def create(db, functions=None):
     if functions is None:
         functions = MATH
-    for name, func in functions.items():
-        db.create_function(name, len(signature(math.pow).parameters), func)
+    for args in functions:
+        db.create_function(*args)
